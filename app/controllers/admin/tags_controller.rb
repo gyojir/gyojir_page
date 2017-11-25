8ed8f1736b2aa379087f6909201db317909f6490
gyojir <youjiro205@gmail.com>
2017-11-26 08:52:40 +0900

    add admin namespace

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,18 +1,16 @@
-class TagsController < ApplicationController
-  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
-
+class Admin::TagsController < Admin::Base
   def create
     @manga = Manga.find(params[:manga_id])
     @tag = @manga.tags.create(tag_params)
-    redirect_to edit_manga_path(@manga)
+    redirect_to edit_admin_manga_path(@manga)
   end
 
   def destroy
     @manga = Manga.find(params[:manga_id])
     @tag = @manga.tags.find(params[:id])
     @tag.destroy
-    redirect_to manga_path(@manga)
-  def
+    redirect_to admin_manga_path(@manga)
+  end
 
   private
   def tag_params
