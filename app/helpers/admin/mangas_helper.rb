9138a2ce8fc93288baa8d92e585bd83d1dc03372
gyojir <gyojir@users.noreply.github.com>
2017-11-26 08:52:40 +0900

    add admin namespace

---
+++ app/helpers/admin/mangas_helper.rb
@@ -0,0 +1,2 @@
+module Admin::MangasHelper
+end
---
+++ test/controllers/admin/mangas_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admin::MangasControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
