03febb9fd28cde6a9417e3dc0ac6b37ddf90b17b
gyojir <gyojir@users.noreply.github.com>
2017-11-27 02:37:58 +0900

    change cloud storage to cloudinary

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,6 +3,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  if Rails.env == 'production'
+    include Cloudinary::CarrierWave
+  end
 
   # Choose what kind of storage to use for this uploader:
   storage :file
