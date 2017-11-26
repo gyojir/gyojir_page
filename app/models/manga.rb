8a14ade0174a70e6270ae91a2a6e2fcd6efadfa1
gyojir <youjiro205@gmail.com>
2017-11-26 21:22:11 +0900

    change image uploader to carrierwave

--- app/models/manga.rb
+++ app/models/manga.rb
@@ -1,6 +1,6 @@
 class Manga < ApplicationRecord
   has_many :tags, dependent: :destroy
   accepts_nested_attributes_for :tags, allow_destroy: true
+  mount_uploader :image, ImageUploader
   validates :title, presence: true
-  validates :path, presence: true
 end
