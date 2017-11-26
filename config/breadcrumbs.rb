f426f7f3e2bae62428862d68a17596584d9fd559
gyojir <gyojir@users.noreply.github.com>
2017-11-27 04:41:30 +0900

    use gretel for breadcrumb list

---
+++ config/breadcrumbs.rb
@@ -0,0 +1,46 @@
+crumb :root do
+  link "Home", root_path
+end
+
+crumb :about do
+  link "About", about_path
+
+  parent :root
+end
+
+crumb :mangas do
+  link "漫画", mangas_path
+
+  parent :root
+end
+
+crumb :manga do |manga|
+  link manga.title, manga
+
+  parent :mangas
+end
+
+# crumb :projects do
+#   link "Projects", projects_path
+# end
+
+# crumb :project do |project|
+#   link project.name, project_path(project)
+#   parent :projects
+# end
+
+# crumb :project_issues do |project|
+#   link "Issues", project_issues_path(project)
+#   parent :project, project
+# end
+
+# crumb :issue do |issue|
+#   link issue.title, issue_path(issue)
+#   parent :project_issues, issue.project
+# end
+
+# If you want to split your breadcrumbs configuration over multiple files, you
+# can create a folder named `config/breadcrumbs` and put your configuration
+# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
+# folder are loaded and reloaded automatically when you change them, just like
+# this file (`config/breadcrumbs.rb`).
