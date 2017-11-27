5218c5cdd5f7c4c6e8655fd22e50037e4542b804
gyojir <gyojir@users.noreply.github.com>
2017-11-27 19:06:00 +0900

    remove order model

--- app/models/manga.rb
+++ app/models/manga.rb
@@ -3,4 +3,9 @@ class Manga < ApplicationRecord
   accepts_nested_attributes_for :tags, allow_destroy: true
   mount_uploader :image, ImageUploader
   validates :title, presence: true
+
+  ORDERS = {
+    '新しい順' => 'created_at desc',
+    '古い順' => 'created_at asc'
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127082346) do
+ActiveRecord::Schema.define(version: 20171126115658) do
 
   create_table "mangas", force: :cascade do |t|
     t.string "title"
@@ -20,13 +20,6 @@ ActiveRecord::Schema.define(version: 20171127082346) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
-  end
-
-  create_table "orders", force: :cascade do |t|
-    t.string "name"
-    t.string "expression"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "tags", force: :cascade do |t|
