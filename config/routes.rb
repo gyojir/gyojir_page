993ed96a7cff702c10c3ede41c247d3ac5257fa5
gyojir <gyojir@users.noreply.github.com>
2017-12-08 19:59:36 +0900

    bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/link', to: 'static_pages#link'
 
   if Rails.env == 'production'
-    get 'sitemap.xml', to:  Cloudinary::Utils.cloudinary_url("sitemap.xml", :resource_type => :raw)
+    get 'sitemap.xml', to: redirect(Cloudinary::Utils.cloudinary_url("sitemap.xml", :resource_type => :raw))
   end
 
   resources :mangas do
