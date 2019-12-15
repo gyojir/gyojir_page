690fe325c2a5ab50cc89d5506296aeb7db86d015
後藤 <gyojir@users.noreply.github.com>
2019-12-15 22:59:53 +0900

    change folder.

--- app/controllers/admin/base.rb
+++
@@ -1,9 +0,0 @@
-class Admin::Base < ApplicationController
-  before_action :basic_auth
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |user, pass|
-      user == ENV["BASIC_AUTH_USER"] && pass == ENV["BASIC_AUTH_PASS"]
-    end
-  end
-end
