6b7a6222ef3efcaeba74f0ab105c7f43753076a7
gyojir <gyojir@users.noreply.github.com>
2020-04-08 13:02:23 +0900

    remove files

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get '/tools', to: 'static_pages#tools'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  get '/link', to: 'static_pages#link'
 
   if Rails.env == 'production'
     get 'sitemap.xml', to: redirect(Cloudinary::Utils.cloudinary_url("sitemap.xml", :resource_type => :raw))
